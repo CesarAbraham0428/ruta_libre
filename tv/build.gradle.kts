81662e4c9fb2a4b9d56b2718ba2262c97b65c6d8
CesarAbraham0428 <lopezcesar0428@gmail.com>
2026-07-20 23:32:41 -0600

    Fix: Aumentar minSdk a 24 en modulo tv para compatibilidad con modulo core

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
     defaultConfig {
         applicationId = "mx.utng.cala.tv"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
